1b930b8ee1ebe746dd6988e40685d22166957fe9
Alberto Rodriguez <arodu.dev@gmail.com>
2020-03-28 17:35:11 -0400

    update

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const resBtns = document.querySelectorAll('.resButton');
 const resetBtn = document.querySelector('.resetButton');
 const view = document.querySelector('.wrapper');
+const responsiveView = document.querySelector('.responsiveView');
+
+const urlParams = new URLSearchParams(window.location.search);
+
+if( urlParams.has('url') ){
+	let url = urlParams.get('url')
+	responsiveView.src = url;
+
+	let input = document.getElementById('input-url')
+	input.value = url;
+}
 
 resBtns.forEach(button => {
 	button.addEventListener('click', () => {
-		view.style.width = button.value + 'px';
+		view.style.width = button.value;
 		button.classList.add('active');
 		removeActive(button, resBtns);
 	});
